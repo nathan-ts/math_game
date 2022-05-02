69720b204a20407efc089b8ff0d471023f03d892
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-05-02 17:46:09 +0000

    add planning docs

---
+++ main.rb
@@ -0,0 +1,17 @@
+=begin
+
+Nouns???
+Player
+- Holds the score of the player
+- Holds the lives of the player
+Question
+- Auto generates the question to ask (on init)
+- Holds the text of the questions to ask
+Game
+- Holds two Player and some Questions
+- Calculates the winner if any, each turn
+- Accepts user input
+- Advances the game state by 1 each time, tracking the current player
+
+
+=end
